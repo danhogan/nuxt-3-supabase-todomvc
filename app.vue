8935be58a2c644225498d037e85b2df2813571a1
Dan Hogan <danhogan78@gmail.com>
2022-02-05 00:02:49 -0500

    init env variables

--- app.vue
+++ app.vue
@@ -3,3 +3,13 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup>
+const config = useRuntimeConfig();
+
+// import { createClient } from '@supabase/supabase-js'
+
+// const supabaseUrl = 'https://euqmldhpbztjixguprns.supabase.co'
+// const supabaseKey = process.env.SUPABASE_KEY
+// const supabase = createClient(supabaseUrl, supabaseKey)
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+    publicRuntimeConfig: {
+        supabaseUrl: process.env.SUPABASE_URL
+    },
+    privateRuntimeConfig: {
+        supabaseKey: process.env.SUPABASE_KEY
+    }
 })
